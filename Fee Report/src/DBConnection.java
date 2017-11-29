ba016124bd7cdcca3debcac391e42fee16dc6dcf
Mukund Bhole <mbhole444@gmail.com>
2017-11-29 18:22:16 +0530

    Add files via upload

---
+++ Fee Report/src/DBConnection.java
@@ -0,0 +1,34 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author AppSidd
+ */
+public class DBConnection {
+    static Connection con;
+     public static Connection ConnectionDB()
+    {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con=DriverManager.getConnection("jdbc:mysql://localhost/feereport", "root", "");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return con;
+        
+    }        
+    
+}
